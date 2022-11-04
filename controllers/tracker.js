1a7419b409c99550686c9ebace66eb3c7c55ce42
cps97 <colinstoutt@gmail.com>
2022-11-04 16:22:52 -0700

    add tracker controller

---
+++ controllers/tracker.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const trackerRouter = express.Router();
+const session = require("express-session");
+const Round = require("../models/round.js");
+const User = require("../models/user.js");
+
+// index/dashboard
+trackerRouter.get("/", (req, res) => {
+  if (req.session.currentUser) {
+    Round.find({}, (err, allRounds) => {
+      res.render("rounds/dashboard.ejs", {
+        rounds: allRounds,
+        currentUser: req.session.currentUser,
+        tabTitle: "Dashboard",
+      });
+    });
+  } else {
+    res.render("rounds/index.ejs", {
+      currentUser: req.session.currentUser,
+      tabTitle: "Log In",
+    });
+  }
+});
+
+// new
+trackerRouter.get("/new", (req, res) => {
+  res.render("rounds/new.ejs", {
+    currentUser: req.session.currentUser,
+    tabTitle: "Add Round",
+  });
+});
+
+// delete
+
+// update
+
+// create
+trackerRouter.post("/", (req, res) => {
+  User.findById(req.session.currentUser, (err, foundUser) => {
+    Round.create(req.body, (err, createdRound) => {
+      foundUser.rounds.push(createdRound);
+      foundUser.save((err, data) => {
+        res.redirect("/tracker");
+      });
+    });
+  });
+});
+// -=-create-=-
+// productRouter.post("/", (req, res) => {
+//     // actually adding the data to MongoDB Atlas
+//     Product.create(req.body, (err, createdProduct) => {
+//       res.redirect("/products");
+//     });
+//   });
+
+// edit
+
+// show
+
+module.exports = trackerRouter;
