b209a4139587e50a7973ec854861891049e74afb
cps97 <colinstoutt@gmail.com>
2022-11-04 16:30:26 -0700

    add rounds document

--- models/user.js
+++ models/user.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Round = require("./round");
 
 const userSchema = Schema({
   email: { type: String, unique: true, required: true },
   //   username: {type: String, unique: true, required: true},
   password: { type: String, required: true },
+  rounds: [Round.schema],
 });
 // ```` unique: true ``` = keeps the mongo _id/key the same, same data cannot be posted twice, doing so will result in an error
 const User = mongoose.model("User", userSchema);
