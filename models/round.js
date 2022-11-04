6be0139b2a7b53a1f1196320c31bed4d1a482b96
cps97 <colinstoutt@gmail.com>
2022-11-04 16:24:15 -0700

    add round schema

---
+++ models/round.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const roundSchema = Schema({
+  course: { type: String, required: true },
+  date: { type: Date, required: true },
+  handicap: { type: Number, default: 0 },
+  par: [Number],
+  score: [Number],
+});
+
+const Round = mongoose.model("Round", roundSchema);
+
+module.exports = Round;
